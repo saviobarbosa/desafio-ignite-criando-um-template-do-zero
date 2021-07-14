27475dd1d63dc98f6153735108b9640cfbe8f076
Savio Barbosa <saviiobarbosa@gmail.com>
2021-07-14 10:33:51 -0300

    teste

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,20 +1,23 @@
-import format from 'date-fns/format';
 import { GetStaticPaths, GetStaticProps } from 'next';
-
-import Prismic from '@prismicio/client';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 
-import Head from 'next/head';
+import format from 'date-fns/format';
+import { ptBR } from 'date-fns/locale';
 import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
+
+import Prismic from '@prismicio/client';
 import { RichText } from 'prismic-dom';
+import Link from 'next/link';
 import { getPrismicClient } from '../../services/prismic';
 
-// import commonStyles from '../../styles/common.module.scss';
-import styles from './post.module.scss';
 import Header from '../../components/Header';
 
-interface Posts {
+import styles from './post.module.scss';
+
+interface Post {
   first_publication_date: string | null;
+  last_publication_date: string | null;
   data: {
     title: string;
     banner: {
@@ -31,84 +34,145 @@ interface Posts {
 }
 
 interface PostProps {
-  post: Posts;
+  post: Post;
+  navigation: {
+    prevPost: {
+      uid: string;
+      data: {
+        title: string;
+      };
+    }[];
+    nextPost: {
+      uid: string;
+      data: {
+        title: string;
+      };
+    }[];
+  };
 }
 
-export default function Post({ post }: PostProps) {
-  const { isFallback } = useRouter();
+export default function Post({ post, navigation }: PostProps) {
+  // TODO
+  const router = useRouter();
 
-  function getReadingTime() {
-    const content = post.data.content.reduce((text, cont) => {
-      text += `${cont.heading}`;
-
-      const body = RichText.asText(cont.body);
-
-      text += body;
-
-      return text;
-    }, '');
-
-    const wordCount = content.split(/\s/).length;
-
-    return Math.ceil(wordCount / 200);
-  }
-
-  if (isFallback) {
+  if (router.isFallback) {
     return <h1>Carregando...</h1>;
   }
+
+  const totalWords = post.data.content.reduce((total, contentItem) => {
+    total += contentItem.heading.split(' ').length;
+
+    const words = contentItem.body.map(item => item.text.split(' ').length);
+    words.map(word => (total += word));
+
+    return total;
+  }, 0);
+
+  const readingTime = Math.ceil(totalWords / 200);
+
+  const formatedFirstPublicationDate = format(
+    new Date(post.first_publication_date),
+    'dd MMM yyyy',
+    { locale: ptBR }
+  );
+
+  const formatedLastPublicationDate = format(
+    new Date(post.last_publication_date),
+    'dd MMM yyyy',
+    { locale: ptBR }
+  );
+
+  const formatedLastPublicationHour = format(
+    new Date(post.last_publication_date),
+    'hh:mm',
+    { locale: ptBR }
+  );
 
   return (
     <>
       <Head>
-        <title>{post.data.title} | space-traveling</title>
+        <title>{`${post.data.title} | spacetraveling`}</title>
       </Head>
 
       <Header />
 
-      <img className={styles.banner} src={post.data.banner.url} alt="" />
+      <img
+        src={post.data.banner.url}
+        alt={post.data.title}
+        className={styles.banner}
+      />
+      <div className={styles.container}>
+        <article>
+          <header>
+            <h1>{post.data.title}</h1>
 
-      <main>
-        <article className={styles.post}>
-          <h1>{post.data.title}</h1>
-          <div className={styles.postInfo}>
-            <time>
-              <FiCalendar size={16} />
-              {post.first_publication_date}
-            </time>
-            <span>
-              <FiUser size={16} />
-              {post.data.author}
-            </span>
-            <span>
-              <FiClock size={16} />
-              {getReadingTime()} min
-            </span>
-          </div>
+            <footer>
+              <div>
+                <FiCalendar size="1.25rem" className={styles.icon} />
+                <time>{formatedFirstPublicationDate}</time>
+              </div>
+              <div>
+                <FiUser size="1.25rem" className={styles.icon} />
+                <p>{post.data.author}</p>
+              </div>
+              <div>
+                <FiClock size="1.25rem" className={styles.icon} />
+                <p>{`${readingTime} min`}</p>
+              </div>
+            </footer>
+          </header>
 
-          {post.data.content.map(content => (
-            <section key={content.heading}>
-              <h2>{content.heading}</h2>
+          {post.data.content.map((item, index) => (
+            <div key={item.heading}>
+              <h2>{item.heading}</h2>
               <div
-                dangerouslySetInnerHTML={{
-                  __html: RichText.asHtml(content.body),
-                }}
+                className={styles.postContent}
+                dangerouslySetInnerHTML={{ __html: RichText.asHtml(item.body) }}
               />
-            </section>
+            </div>
           ))}
         </article>
-      </main>
+
+        <hr />
+
+        <section className={styles.navigation}>
+          {navigation?.prevPost.length > 0 && (
+            <div className={styles.prevPost}>
+              <h3>{navigation.prevPost[0].data.title}</h3>
+              <Link href={`/post/${navigation.prevPost[0].uid}`}>
+                <a>Post anterior</a>
+              </Link>
+            </div>
+          )}
+
+          {navigation?.nextPost.length > 0 && (
+            <div className={styles.nextPost}>
+              <h3>{navigation.nextPost[0].data.title}</h3>
+              <Link href={`/post/${navigation.nextPost[0].uid}`}>
+                <a>Próximo post</a>
+              </Link>
+            </div>
+          )}
+        </section>
+      </div>
     </>
   );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const prismic = getPrismicClient();
-  const posts = await prismic.query(
-    [Prismic.Predicates.at('document.type', 'posts')],
-    { pageSize: 1, fetch: ['posts.uid'] }
-  );
+  const posts = await prismic.query([
+    Prismic.predicates.at('document.type', 'posts'),
+  ]);
 
-  const paths = posts.results.map(post => ({ params: { slug: post.uid } }));
+  // TODO
+  const paths = posts.results.map(post => {
+    return {
+      params: {
+        slug: post.uid,
+      },
+    };
+  });
 
   return {
     paths,
@@ -116,23 +180,39 @@ export const getStaticPaths: GetStaticPaths = async () => {
   };
 };
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
+export const getStaticProps: GetStaticProps = async ({
+  params,
+  previewData,
+}) => {
   const { slug } = params;
-
   const prismic = getPrismicClient();
-  const response = await prismic.getByUID('posts', String(slug), {});
+  const response = await prismic.getByUID('posts', String(slug), {
+    ref: previewData?.ref || null,
+  });
 
-  const content = response.data.content.map(cont => ({
-    heading: cont.heading,
-    body: cont.body,
-  }));
+  const prevPost = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.first_publication_date]',
+    }
+  );
 
+  const nextPost = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.first_publication_date desc]',
+    }
+  );
+
+  // TODO
   const post = {
     uid: response.uid,
-    first_publication_date: format(
-      new Date(response.first_publication_date),
-      'd MMM y'
-    ),
+    first_publication_date: response.first_publication_date,
+    last_publication_date: response.last_publication_date,
     data: {
       title: response.data.title,
       subtitle: response.data.subtitle,
@@ -140,13 +220,23 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
         url: response.data.banner.url,
       },
       author: response.data.author,
-      content,
+      content: response.data.content.map(item => {
+        return {
+          heading: item.heading,
+          body: [...item.body],
+        };
+      }),
     },
   };
 
   return {
     props: {
       post,
+      navigation: {
+        prevPost: prevPost?.results,
+        nextPost: nextPost?.results,
+      },
     },
+    revalidate: 60 * 30, // 30 minutes
   };
 };
